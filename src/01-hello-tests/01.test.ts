ae81992f31f307666d58768b32aaefecf38ebdca
Viktoriyadegt <vika.degtyareva.12@mail.ru>
2023-03-29 10:14:19 +0300

    testing(sum, mult)

---
+++ src/01-hello-tests/01.test.ts
@@ -0,0 +1,39 @@
+import {mult, sum} from "./01";
+
+let a: number
+let b: number
+let c: number
+
+beforeEach(() => {
+    a = 1
+    b = 2
+    c = 3
+})
+
+test('sum should be correct', () => {
+    //data
+   /* const a = 1;
+    const b = 2;
+    const c = 3;*/
+
+    //action
+    const result1 = sum(a, b)
+    const result2 = sum(b, c)
+
+    //expect
+    expect(result1).toBe(3)
+    expect(result2).toBe(5)
+})
+
+test('multiply should be  correct', () => {
+    //data
+
+    //action
+    const result1 = mult(a, b)
+    const result2 = mult(b, c)
+
+    //expect
+    expect(result1).toBe(2)
+    expect(result2).toBe(6)
+})
+
---
+++ src/01-hello-tests/01.ts
@@ -0,0 +1,12 @@
+
+
+export const sum = (a: number, b: number) => {
+
+    return a + b
+}
+
+export const mult = (a: number, b: number) => {
+
+    return a * b
+}
+
